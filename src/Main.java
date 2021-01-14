8ee65dcbdd64de5c758ce78dfc2acb1996b91fe2
cjrumble <cjrumble@users.noreply.github.com>
2021-01-14 12:08:34 -0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // test
+        DayOfTheWeekChallenge.printDayOfTheWeek(1);
+        DayOfTheWeekChallenge.printDayOfTheWeek(2);
+        DayOfTheWeekChallenge.printDayOfTheWeek(3);
+        DayOfTheWeekChallenge.printDayOfTheWeek(4);
+        DayOfTheWeekChallenge.printDayOfTheWeek(5);
+        DayOfTheWeekChallenge.printDayOfTheWeek(6);
+        DayOfTheWeekChallenge.printDayOfTheWeek(7);
+        DayOfTheWeekChallenge.printDayOfTheWeek(8);
+    }
+
+}
